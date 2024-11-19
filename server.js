9073f3153fac6406c898a39ad01116e796b5bac3
J'son David <jdavid@TD425.WPA.Dal.Ca>
2024-11-19 10:13:28 -0400

    Put comments over all the code

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
+/* this pulls in the express  library*/
 const express = require("express");
-const jwt = require("jsonwebtoken");
+/*this runs the express function  */
 const app = express();
-
+/* setting the name port to be at 3000 */
 const PORT = process.env.PORT || 3000;
-
+/* call in the jsonwebtoken lirbrary for authentication */
+const jsw = require(`jsonwebtoken`);
+/* helping the express understand json */
 app.use(express.json());
+/* making users in the api */
+const posts = [
+/* first user in posts */ 
+    {
+        username: "Javon",
+        title: "post"
+    },
+    /* second user in posts */
+    {
+        username: "Bob",
+        title: "post2"
+    }
+]
 
-app.listen(PORT, function () {
-    console.log(`App is listening at ${PORT}`);
+/* prints out the users that are in the posts container */
+app.get(`/posts`, (req, res) => {
+ /* its taking the data from post convers it into json and sends it as a responce to the client */
+    res.json(posts)
 })
-//This is a post request to creat a new user
-app.post(`/post`, function (req,res){
-const user = {id: 1};
-const token = jwt.sign(`my_secret_key`);
-res.json({
-    token: token
+/* looking for the responce in the body of the username */
+app.post("/login", (req, res) => {
+    const username = res.body.username
 })
-})
+
+/* the server will run at port 3000 on at localhost:3000 */
+app.listen(PORT)
+
+
