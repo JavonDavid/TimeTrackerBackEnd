bfc885a539a3f4a29d331a1f3107913f0a866bc1
J'son David <javondavid6@gmail.com>
2024-12-12 11:25:33 -0400

    Changed a word in one of my comments in the auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,12 @@ const SECRET_KEY = "mySecretKey";
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.header["Authorization"];/* this is accessing the header and extracts the value of authorization from imcoming http request  */
-  const token = authHeader.split(" ")[1];/* this is spliting the token so we only get tthe token */
+  const token = authHeader.split(" ")[1];/* this is spliting the token so we only get the token */
   try {
     jwt.verify(token, SECRET_KEY);/* this is verifying the token and signing it with the SECRET_KEY */
     next();/*  gives control to the next route handler*/
   } catch {
-    res.status(401).send({ message: "Invalid Token" });/* if there is and error run send this massage */
+    res.status(401).send({ message: "Invalid Token" });/* if there is and error send this massage */
   }
 };
 
